55402c11cb5944fbe75be3aedf3e6500d6f50c92
Yubil-Olga <Yubil-Olga@yandex.ru>
2020-02-09 01:20:27 +0300

    dropdown for rooms and persons

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -23,31 +23,57 @@ function dropdown(path, totalCounter){
                 el.querySelector('.counter-item').value = counter;
                 totalCounter--;
             }
-            path.querySelector('.dropdown__selection p').textContent = titleCounter(totalCounter);
+            if (totalCounter>0 && path.querySelectorAll('.clean').length>0) {
+                path.querySelector('.clean').style.visibility="visible";
+            }
+            if (totalCounter == 0 && path.querySelectorAll('.clean').length>0) {
+                path.querySelector('.clean').style.visibility="hidden";
+            }
+            path.querySelector('.dropdown__title').textContent = titleCounter(totalCounter, path);
         })
     })
+    if (path.querySelectorAll('.clean').length>0) {
+        path.querySelector('.clean').addEventListener('click', () => {
+            totalCounter = 0;
+            path.querySelector('.dropdown__title').textContent = titleCounter(totalCounter, path);
+            path.querySelector('.clean').style.visibility="hidden";
+            path.querySelectorAll('.counter-item').forEach((item)=> {
+                item.value = 0;
+            })
+        })
+    } 
 }
 
-function titleCounter(total) {
+function titleCounter(total, path) {
     let word ="";
-    let title = "Количество гостей";
-    switch(total) {
-        case 0:
-            return title;
-        case 1:
-            word= "гость";
-            title = `${total} ${word}`;
-            break;
-        case 2:
-        case 3:
-        case 4:
-            word= "гостя";
-            title = `${total} ${word}`;
-            break;
-        default:
-            word= "гостей";
-            title = `${total} ${word}`;
-        
+    let title="";
+    if (path.querySelector('.dropdown__title').classList.contains('room')){
+        path.querySelectorAll('.menu__option').forEach(el => {
+            title += el.querySelector('.counter-item').value +" "+ el.querySelector('.option__title').textContent + ", ";
+        })
+        if (total ==0) {
+            title= "Сколько комнат"
+        }
+    }
+    else {
+        title = "Сколько гостей";
+        switch(total) {
+            case 0:
+                return title;
+            case 1:
+                word= "гость";
+                title = `${total} ${word}`;
+                break;
+            case 2:
+            case 3:
+            case 4:
+                word= "гостя";
+                title = `${total} ${word}`;
+                break;
+            default:
+                word= "гостей";
+                title = `${total} ${word}`;
+    }
     }
     return title;
 }
