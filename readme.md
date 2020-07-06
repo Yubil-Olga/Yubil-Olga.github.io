9e456161e3f5b3993475bb79e325e8368b93ea92
Yubil-Olga <Yubil-Olga@yandex.ru>
2020-07-06 13:07:11 +0300

    social: links, advantages: border-bottom, chart: margin #6

--- readme.md
+++ readme.md
@@ -2,23 +2,23 @@
 
 ### Команды для запуска проекта
 
- `https://github.com/Yubil-Olga/Education` - клонировать репозиторий
+`https://github.com/Yubil-Olga/Education` - клонировать репозиторий
 
- `npm install` - установить зависимости
+`npm install` - установить зависимости
 
- `npm start` - запустить dev server
+`npm start` - запустить dev server
 
- `npm run dev` - запустить сборку(development)
+`npm run dev` - запустить сборку(development)
 
- `npm run build` - запустить сборку(production)
+`npm run build` - запустить сборку(production)
 
 ### Сторонние библиотеки
 
- [air-datepicker](http://t1m0n.name/air-datepicker/docs/index-ru.html)
+[air-datepicker](http://t1m0n.name/air-datepicker/docs/index-ru.html)
 
- [inputmask](https://www.npmjs.com/package/inputmask)
+[inputmask](https://www.npmjs.com/package/inputmask)
 
- [ion-rangeslider](http://ionden.com/a/plugins/ion.rangeSlider/)
+[ion-rangeslider](http://ionden.com/a/plugins/ion.rangeSlider/)
 
 ### Страницы
 * [Landing page](https://yubil-olga.github.io/Education/)
@@ -27,6 +27,6 @@
 * [Room details](https://yubil-olga.github.io/Education/room-details)
 * [Preferences](https://yubil-olga.github.io/Education/search-room.html)
 * UI-kit
-    * [Form-elements](https://yubil-olga.github.io/Education/form-elements)
-    * [Cards](https://yubil-olga.github.io/Education/cards)
-    * [Headers&Footers](https://yubil-olga.github.io/Education/headers-footers)
+  * [Form-elements](https://yubil-olga.github.io/Education/form-elements)
+  * [Cards](https://yubil-olga.github.io/Education/cards)
+  * [Headers&Footers](https://yubil-olga.github.io/Education/headers-footers)
--- webpack.config.js
+++ webpack.config.js
@@ -1,146 +1,147 @@
-const path = require('path')
-const webpack = require('webpack')
-const CopyPlugin = require('copy-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const isDev = process.env.NODE_ENV === 'development'
+
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        index: './pages/index/index.js'
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    index: './pages/index/index.js',
+  },
+  output: {
+    filename: './js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4200,
+    hot: isDev,
+    stats: 'errors-only',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    output: {
-        filename: './js/[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 4200,
-        hot: isDev,
-        stats: 'errors-only'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './pages/index/index.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'headers-footers.html',
+      template: './pages/headers-footers/headers-footers.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'form-elements.html',
+      template: './pages/form-elements/form-elements.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'cards.html',
+      template: './pages/cards/cards.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'registration.html',
+      template: './pages/registration/registration.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'login.html',
+      template: './pages/login/login.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'search-room.html',
+      template: './pages/search-room/search-room.pug',
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'room-details.html',
+      template: './pages/room-details/room-details.pug',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/images'),
+        to: path.resolve(__dirname, 'dist/images'),
       },
-    optimization: {
-        splitChunks: {
-          chunks: 'all',
-          },
-        },
-    plugins: [
-        new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: './pages/index/index.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'headers-footers.html',
-          template: './pages/headers-footers/headers-footers.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'form-elements.html',
-          template: './pages/form-elements/form-elements.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'cards.html',
-          template: './pages/cards/cards.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'registration.html',
-          template: './pages/registration/registration.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'login.html',
-          template: './pages/login/login.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'search-room.html',
-          template: './pages/search-room/search-room.pug'
-        }),
-        new HTMLWebpackPlugin({
-          filename: 'room-details.html',
-          template: './pages/room-details/room-details.pug'
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-        new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
-          "window.jQuery": 'jquery'
-        }),
-        new CopyPlugin([
+    ]),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicons'),
+        to: path.resolve(__dirname, 'dist/favicons'),
+      },
+    ]),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
           {
-            from: path.resolve(__dirname, 'src/images'),
-            to: path.resolve(__dirname, 'dist/images')
-          }
-        ]),
-        new CopyPlugin([
-          { 
-            from: path.resolve(__dirname, 'src/favicons'),
-            to: path.resolve(__dirname, 'dist/favicons') 
-          }
-        ]),
-        new OptimizeCssAssetsPlugin({
-          assetNameRegExp: /\.css$/g,
-          cssProcessor: require('cssnano'),
-          cssProcessorPluginOptions: {
-            preset: ['default', { discardComments: { removeAll: true } }],
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
           },
-          canPrint: false
-        })
+        ],
+      },
+      {
+        test: /\.(png|jpeg|jpg|svg|gif)$/i,
+        exclude: [
+          path.resolve(__dirname, 'src/fonts/'),
+        ],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        include: [
+          path.resolve(__dirname, 'src/fonts/'),
+        ],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+
     ],
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader', 
-                    'resolve-url-loader', 
-                    {
-                      loader: 'sass-loader',
-                      options: {
-                        sourceMap: true
-                      }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpeg|jpg|svg|gif)$/i,
-                exclude: [
-                  path.resolve(__dirname, "src/fonts/")
-                ],
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                        name: 'images/[name].[ext]'
-                        }
-                  },
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                include: [
-                  path.resolve(__dirname, "src/fonts/")
-                ],
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: 'fonts/[name].[ext]'
-                    }
-                  }
-                ]
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader'
-            }
-            
-        ]
-    }
-}
+  },
+};
